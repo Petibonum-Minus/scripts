25dc662a21e08897219665a16819caedbf4ab7ec
Petibonum <62618615+Iole-Bri@users.noreply.github.com>
2021-11-30 15:36:43 -0500

    Update gtfobins.sh

--- gtfobins.sh
+++ gtfobins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-find / -user root -perm /4000 2>/dev/null >> priv1.txt
+# S'il y a internet sur l'hôte victime alors décommenter la ligne suivante :
+
+#find / -user root -perm /4000 2>/dev/null >> priv1.txt
+
 cat priv1.txt | sed -e 's,/, ,g' >> priv2.txt
 grep -oE '[^ ]+$' priv2.txt >> priv3.txt
 sed 's/[[:blank:]]*$//;s/.*[[:blank:]]//' priv3.txt >> priv4.txt
