5c633eab674663c2a4928e99f6b6dcf5f97ccc1e
Iole-Bri <eloibriere@gmail.com>
2021-12-07 16:41:07 -0500

    tmux.md

--- tmux/tmu.sh
+++ tmux/tmu.sh
@@ -19,9 +19,11 @@ tmux rename-window -t $session:1 "Monitor"
 # split first pane horizontal, then second pane vertical
 tmux split-window -t $session:1.1 -h
 tmux split-window -t $session:1.2 -v -l 80%
-tmux send-keys -t $session:Monitor.1 "sudo nmap -Pn -sS -T5 $ip" C-m
-tmux send-keys -t $session:Monitor.2 "while sleep 1; do clear; ip -br a | grep -v veth ; done" C-m
-tmux send-keys -t $session:Monitor.3 "ping $ip" C-m
+tmux split-window -t $session:1.1 -v -l 50%
+tmux send-keys -t $session:Monitor.1 "sudo openvpn VPN/petibonum.minus.ovpn" C-m
+tmux send-keys -t $session:Monitor.2 "ping $ip" C-m
+tmux send-keys -t $session:Monitor.3 "while sleep 1; do clear; ip -br a | grep -v veth ; done" C-m
+tmux send-keys -t $session:Monitor.4 "sudo nmap -Pn -sS -T5 $ip" C-m
 
 # create new named window
 tmux new-window -t $session -n "Main"
