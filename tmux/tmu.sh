653d29b36e8677f576d01b1738718ab1861bdce2
Iole-Bri <eloibriere@gmail.com>
2021-12-07 16:52:17 -0500

    tmux.md

--- tmux/tmu.sh
+++ tmux/tmu.sh
@@ -6,7 +6,9 @@ echo IP ?
 read ip
 
 # VPN ,
-echo VPN ? (THM or HTB)
+THM='THM'
+HTB='HTB'
+echo VPN ? '(THM or HTB, uppercase)'
 read vpn
 
 # name of session
@@ -21,14 +23,14 @@ tmux new-session -s $session -d
 tmux rename-window -t $session:1 "Monitor"
 # split first pane horizontal, then second pane vertical
 tmux split-window -t $session:1.1 -h
-tmux split-window -t $session:1.2 -v -l 80%
+tmux split-window -t $session:1.2 -v -l 70%
 tmux split-window -t $session:1.1 -v -l 50%
 tmux send-keys -t $session:Monitor.2 "ping $ip" C-m
 tmux send-keys -t $session:Monitor.3 "while sleep 1; do clear; ip -br a | grep -v veth ; done" C-m
 tmux send-keys -t $session:Monitor.4 "sudo nmap -Pn -sS -T5 $ip" C-m
 
 #VPN choice
-if $vpn = THM
+if [ "$vpn" -eq "$THM" ]; then
     tmux send-keys -t $session:Monitor.1 "sudo openvpn VPN/petibonum.minus.ovpn" C-m
 else
     tmux send-keys -t $session:Monitor.1 "sudo openvpn VPN/lab" C-m
