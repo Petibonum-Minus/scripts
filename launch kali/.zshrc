3350bc081a7481f34ba12f7c3c30579246b77f8c
Iole-Bri <eloibriere@gmail.com>
2021-12-08 06:56:55 -0500

    launch.sh

---
+++ launch kali/.zshrc
@@ -0,0 +1,5 @@
+# others aliases
+alias install='sudo apt-get install -y'
+alias update='sudo apt-get update && sudo apt-get full-upgrade -y'
+alias purge='sudo apt-get purge -y'
+alias fix='sudo apt-get --fix-broken install -y'
--- launch kali/launch.sh
+++ launch kali/launch.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+#sudo apt-get update && sudo apt-get full-upgrade -y
+#sudo apt-get install -y git-all git-clone
+
+#sudo cp ~/.zshrc ~/.zshrc.backup
+
+echo "# others aliases" >> ~/.zshrc
+echo "alias install='sudo apt-get install -y'" >> ~/.zshrc
+echo "alias update='sudo apt-get update && sudo apt-get full-upgrade -y'" >> ~/.zshrc
+echo "alias purge='sudo apt-get purge -y'" >> ~/.zshrc
+echo "alias fix='sudo apt-get --fix-broken install -y'" >> ~/.zshrc
+#source ~/.zshrc
+
+mkdir ~/Tools
+mkdir ~/Pentest
+
+git clone https://github.com/Iole-Bri/scripts.git ~/Tools
+git clone https://github.com/Iole-Bri/Petibonum_docs.git ~/Documents
