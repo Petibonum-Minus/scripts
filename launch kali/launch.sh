6c94875dafacb4e398fbab5bfec480179ad67540
Iole-Bri <petibonum.minus@protonmail.com>
2021-12-14 08:05:30 -0500

    launch

--- launch kali/launch.sh
+++ launch kali/launch.sh
@@ -14,7 +14,6 @@ cd && echo "alias update='sudo apt-get update && sudo apt-get full-upgrade -y'" 
 cd && echo "alias purge='sudo apt-get purge -y'" >> .zshrc
 cd && echo "alias fix='sudo apt-get --fix-broken install -y'" >> .zshrc
 cd && echo "alias clean_remove='sudo apt-get autoremove -y && sudo apt-get autoclean -y'" >> .zshrc
-cd && source .zshrc
 
 # Directory
 mkdir ~/Tools
@@ -24,8 +23,11 @@ mkdir ~/Pentest/HTB/
 mkdir ~/VPN
 
 # Install Tools
-cd Tools && git clone https://github.com/Iole-Bri/scripts.git && git clone https://github.com/volatilityfoundation/volatility3.git && cd volatility3 && sudo python3 setup.py install && cd
-cd Documents && git clone https://github.com/Spit22/GOTHAM.git && https://github.com/Iole-Bri/Petibonum_docs.git && cd
+cd Tools && git clone https://github.com/Iole-Bri/scripts.git && cd
+cd Documents && git clone https://github.com/Spit22/GOTHAM.git && git clone https://github.com/Iole-Bri/Petibonum_docs.git && cd
+cd Tools && wget http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_lin64_standalone.zip && unzip volatility_2.6_lin64_standalone.zip && rm volatility_2.6_lin64_standalone.zip && mv volatility_2.6_lin64_standalone volatility && cd volatility && sudo ln -s /home/kali/Tools/volatility/volatility_2.6_lin64_standalone /usr/bin/volatility && cd
 
 # Install Links
-sudo ln -s ~/Tools/scripts/tmux/tmux.sh /usr/bin/Pentest
+sudo ln -s /home/kali/Tools/scripts/tmux/tmux_1.sh /usr/bin/pentestip
+sudo ln -s /home/kali/Tools/scripts/tmux/tmux_2.sh /usr/bin/pentest
+sudo ln -s /home/kali/Tools/volatility3/vol.py /usr/bin/volatility
